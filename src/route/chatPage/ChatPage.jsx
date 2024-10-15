1c8f2a8f48cd8055e1e84566aba6f81717c51683
Tranhuudong <tranhuudong282001@gmail.com>
2024-10-15 22:13:38 +0700

    React Image Upload and Optimization Tutorial

--- src/route/chatPage/ChatPage.jsx
+++ src/route/chatPage/ChatPage.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import "./ChatPage.css"
+import React, { useEffect, useRef } from "react";
+import "./ChatPage.css";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
-}
+  const endRef = useRef(null);
 
-export default ChatPage
+  useEffect(() => {
+    endRef.current.scrollIntoView({});
+  }, []);
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt />
+
+          <div ref={endRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
