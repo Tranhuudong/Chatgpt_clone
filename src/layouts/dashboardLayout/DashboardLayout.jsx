2b1fcfce332ed5781c2bd29158bbac6095fde401
Tranhuudong <tranhuudong282001@gmail.com>
2024-10-15 21:24:43 +0700

    design  dashboard (layout and page)

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./DashboardLayout.css";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "@clerk/clerk-react";
+import ChatList from "../../components/chatList/ChatList";
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth();
@@ -16,7 +17,7 @@ const DashboardLayout = () => {
   if (!isLoaded) return "Loading...";
   return (
     <div className="dashboardLayout">
-      <div className="menu">MENU</div>
+      <div className="menu"><ChatList /></div>
       <div className="content">
         <Outlet />
       </div>
--- src/route/signUpPage/SignUpPage.jsx
+++ src/route/signUpPage/SignUpPage.jsx
@@ -5,7 +5,11 @@ import { SignUp } from "@clerk/clerk-react";
 const SignUpPage = () => {
   return (
     <div className="signUpPage">
-      <SignUp path="/sign-up" signInUrl="/sign-in" />
+      <SignUp
+        path="/sign-up"
+        signInUrl="/sign-in"
+        forceRedirectUrl="/dashboard"
+      />
     </div>
   );
 };
