ca34913dd11cc54749e8183567d18273b19ea1bd
Tranhuudong <tranhuudong282001@gmail.com>
2024-10-16 22:02:14 +0700

    React AI Chat using Gemini API

--- src/components/newPrompt/NewPrompt.jsx
+++ src/components/newPrompt/NewPrompt.jsx
@@ -2,8 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import "./NewPrompt.css";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/Gemini";
+import Markdown from "react-markdown";
 
 const NewPrompt = () => {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
   const [img, setImg] = useState({
     isLoading: false,
     error: "",
@@ -13,23 +17,45 @@ const NewPrompt = () => {
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
+  }, [question,answer, img.dbData]);
+
+  const add = async (text) => {
+    setQuestion(text);
+
+    const result = await model.generateContent(text);
+    const response = await result.response.text();
+    console.log(result.response.text());
+    setAnswer(response);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const text = e.target.text.value;
+    if (!text) return;
+    add(text);
+  };
   return (
     <>
       {/* ADD NEW CHAT */}
       {img.isLoading && <div className="">Loading...</div>}
       {img.dbData?.filePath && (
-        <IKImage 
-         urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
-         path={img.dbData?.filePath}
-         width="380"
-         transformation={[{with: 380}]}
-        
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width="380"
+          transformation={[{ with: 380 }]}
         />
       )}
+      {question && <div className="message user">{question}</div>}
+      {answer && (
+        <div className="message ">
+          <Markdown>{answer}</Markdown>
+        </div>
+      )}
       <div className="endChat" ref={endRef}></div>
-      <form className="newForm">
-        <Upload setImg={setImg}/>
+      <form className="newForm" onSubmit={handleSubmit}>
+        <Upload setImg={setImg} />
 
         {/* <label htmlFor="file">
           <img src="/attachment.png" alt="" />
